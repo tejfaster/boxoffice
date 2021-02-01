ec12b05d699a02dc786ab8d19d88aebc17144ac3
TEJ PRATAP <tejfaster55@gmail.com>
2021-02-01 23:14:07 +0530

    finally we use reducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,31 +1,53 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useReducer } from 'react'
 import { useParams } from 'react-router-dom'
 import { apiGet } from '../misc/config'
 
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS': {
+            return { isLoading: false, error: null, show: action.show }
+        }
+
+        case 'FETCH_FAILED': {
+            return { ...prevState, isLoading: false, error: action.error, }
+        }
+        default: return prevState
+    }
+}
+
+const initialState = {
+    show: "Component did mount",
+    isLoading: true,
+    error: null
+}
+
 function Show() {
     const { id } = useParams()
-    const [show, setshow] = useState("Component did mount ")
-    const [isLoading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
 
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // const [show, setshow] = useState("Component did mount ")
+    // const [isLoading, setLoading] = useState(true)
+    // const [error, setError] = useState(null)
+    console.log(state)
     useEffect(() => {
 
         let isMounted = true
 
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
             .then(results => {
-
                 setTimeout(() => {
                     if (isMounted) {
-                        setshow(results);
-                        setLoading(false);
+                        dispatch({ type: 'FETCH_SUCCESS', show: results })
+                        // setshow(results);
+                        // setLoading(false);
                     }
                 }, 2000)
             }).catch(err => {
                 if (isMounted) {
-                    setError(err.message)
-                    setLoading(false);
+                    dispatch({ type: 'FETCH_FAILED', error: err.message })
+                    // setError(err.message)
+                    // setLoading(false);
                 }
             })
         return () => {
@@ -33,13 +55,14 @@ function Show() {
         }
     }, [id])
 
-    console.log('show', show)
-    if (isLoading) {
-        return <div>Data is Loading</div>
-    }
-    if (error) {
-        return <div>Error occurred :{error}</div>
-    }
+
+    // console.log('show', show)
+    // if (isLoading) {
+    //     return <div>Data is Loading</div>
+    // }
+    // if (error) {
+    //     return <div>Error occurred :{error}</div>
+    // }
     return <div>this is show page</div>
 }
 

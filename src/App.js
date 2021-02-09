8b0d4196817191570953bec3eb23f2f8f38bf056
TEJ PRATAP <tejfaster55@gmail.com>
2021-02-09 12:48:01 +0530

    defined theme for app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
 import Starred from './pages/Starred'
 import Home from './pages/Home'
 import Show from './pages/Show'
 
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
+
 function App() {
   return (
-   <Switch>
-     <Route exact path="/">
-    <Home/>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
+        <Route>
+          404: not found
      </Route>
-     <Route exact path="/starred">
-      <Starred/>
-     </Route>
-     <Route exact path="/show/:id"> 
-      <Show/>
-     </Route>
-     <Route>
-     404: not found
-     </Route>
-   </Switch>  
+      </Switch>
+    </ThemeProvider>
   );
 }
 
 export default App;
- 
--- src/components/Style.js
+++ src/components/Style.js
@@ -39,11 +39,14 @@ export const Star = styled.div`
   display: inline-block;
   width: 18px;
   height: 18px;
-  background-color: #ffc806;
+  //background-color: #ffc806;
+  background-color:${props =>
+    (props.active ? '#ffc806' : '#ddd')
+  };
   clip-path: polygon(
     50% 0%,
     61% 35%,
-    98% 35%, 
+    98% 35%,
     68% 57%,
     79% 91%,
     50% 70%,
